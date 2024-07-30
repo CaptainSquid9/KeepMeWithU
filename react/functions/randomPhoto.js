eab40097844169afc178d8d3719b85ac32bd9ca7
CaptainSquid9 <83174650+CaptainSquid9@users.noreply.github.com>
2024-07-29 21:48:52 -0400

    You are actually stupid

--- react/functions/randomPhoto.js
+++ react/functions/randomPhoto.js
@@ -1,5 +1,6 @@
 const { google } = require("googleapis");
 const fs = require("fs");
+const path = require("path");
 
 // Load service account credentials
 const keyFile = path.resolve(__dirname, "service.json");
