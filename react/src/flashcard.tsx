032227492e493cec4fde2a9bf57dc8833cd3c910
CaptainSquid9 <83174650+CaptainSquid9@users.noreply.github.com>
2024-07-30 01:49:02 -0400

    Unfinished Map update
    It's resetting all the other elements every time

--- react/src/flashcard.tsx
+++ react/src/flashcard.tsx
@@ -3,65 +3,51 @@ import axios from "axios";
 import "./flashcard.css";
 
 function flashCard() {
+  type ValuesObject = {
+    [key: string]: number; // This allows indexing with numbers
+  };
+  type BoolsObject = {
+    [key: string]: boolean; // This allows indexing with numbers
+  };
+  type StringObject = {
+    [key: string]: string; // This allows indexing with numbers
+  };
+
   //position for each layer
+  //Amount of layers to generate
+  const [Layers, setLayers] = useState<number>(10);
 
   //X
-  const [divX1, setDivX1] = useState<number>(window.innerWidth / 2);
-  const [divX2, setDivX2] = useState<number>(window.innerWidth / 2);
-  const [divX3, setDivX3] = useState<number>(window.innerWidth / 2);
-  const [divX4, setDivX4] = useState<number>(window.innerWidth / 2);
-
-  const setDivX = [setDivX1, setDivX2, setDivX3, setDivX4];
-  const DivX = [divX1, divX2, divX3, divX4];
+  const [divX, setDivX] = useState<ValuesObject>({});
 
   //Y
-  const [divY1, setDivY1] = useState<number>(window.innerHeight / 2);
-  const [divY2, setDivY2] = useState<number>(window.innerHeight / 2);
-  const [divY3, setDivY3] = useState<number>(window.innerHeight / 2);
-  const [divY4, setDivY4] = useState<number>(window.innerHeight / 2);
-
-  const setDivY = [setDivY1, setDivY2, setDivY3, setDivY4];
-  const DivY = [divY1, divY2, divY3, divY4];
+  const [divY, setDivY] = useState<ValuesObject>({});
 
   const [MouseDownBool, setMDBool] = useState<boolean>(false);
 
   //Bool checking if swiped for each layer
-  const [SwipedBool1, setSwipedBool1] = useState<boolean>(false);
-  const [SwipedBool2, setSwipedBool2] = useState<boolean>(false);
-  const [SwipedBool3, setSwipedBool3] = useState<boolean>(false);
-  const [SwipedBool4, setSwipedBool4] = useState<boolean>(false);
-
-  const setSwipedBool = [
-    setSwipedBool1,
-    setSwipedBool2,
-    setSwipedBool3,
-    setSwipedBool4,
-  ];
-  const SwipedBool = [SwipedBool1, SwipedBool2, SwipedBool3, SwipedBool4];
+  const [SwipedBool, setSwipedBool] = useState<BoolsObject>({});
   //Z-index counter for each layer
-  const [Counter1, setCounter1] = useState<number>(1000);
-  const [Counter2, setCounter2] = useState<number>(999);
-  const [Counter3, setCounter3] = useState<number>(998);
-  const [Counter4, setCounter4] = useState<number>(997);
+  const [Counter, setCounter] = useState<ValuesObject>({});
 
-  const setCounter = [setCounter1, setCounter2, setCounter3, setCounter4];
-  const Counter = [Counter1, Counter2, Counter3, Counter4];
   //const [ImgCounterF, setImgCounterF] = useState<number>(1);
   //const [ImgCounterB, setImgCounterB] = useState<number>(2);
 
   // Fetch random photo for each layer
+  const [photoUrl, setPhotoUrl] = useState<StringObject>({});
 
-  const [photoUrl1, setPhotoUrl1] = useState<string | undefined>(undefined);
-  const [photoUrl2, setPhotoUrl2] = useState<string | undefined>(undefined);
-  const [photoUrl3, setPhotoUrl3] = useState<string | undefined>(undefined);
-  const [photoUrl4, setPhotoUrl4] = useState<string | undefined>(undefined);
-
-  const setPhotoUrl = [setPhotoUrl1, setPhotoUrl2, setPhotoUrl3, setPhotoUrl4];
+  for (const elem in Array(Layers)) {
+    setDivX({ ...divX, [elem]: window.innerWidth / 2 });
+    setDivY({ ...divY, [elem]: window.innerHeight / 2 });
+    setSwipedBool({ ...SwipedBool, [elem]: false });
+    setCounter({ ...Counter, [elem]: 1000 - Number(elem) });
+    setPhotoUrl({ ...photoUrl, [elem]: "" });
+  }
 
   //Random photo
   // Called by every layer
 
-  const fetchRandomPhoto = async (id: number) => {
+  const fetchRandomPhoto = async (id: string) => {
     try {
       const response = await axios.get("/api/randomPhoto", {
         headers: {
@@ -71,34 +57,31 @@ function flashCard() {
         responseType: "blob",
       });
       const imageUrl = URL.createObjectURL(response.data);
-      const func = setPhotoUrl[id];
-      func(imageUrl);
+      setPhotoUrl({ ...photoUrl, [id]: imageUrl });
     } catch (error) {
       console.error("Error fetching photo", error);
     }
   };
   useEffect(() => {
-    for (var i = 0; i < 4; i++) {
-      fetchRandomPhoto(i);
+    for (var i = 0; i < Layers; i++) {
+      fetchRandomPhoto(String(i));
     }
   }, []);
 
   //Follow touch
   const positionFollowTouch = (e: React.TouchEvent<HTMLDivElement>) => {
     console.log(e.touches[0].clientX, e.touches[0].clientY, e.currentTarget.id);
-    const id = Number(e.currentTarget.id) - 1;
+    const id = Number(e.currentTarget.id);
     var back;
     if (id == 0) {
-      back = 3;
+      back = Layers;
     } else {
-      back = id - 1;
+      back = id;
     }
-    const funcY = setDivY[id];
-    const funcX = setDivX[id];
 
     if (Counter[id] > Counter[back] && SwipedBool[id] == false) {
-      funcX(e.touches[0].clientX);
-      funcY(e.touches[0].clientY);
+      setDivX({ ...divX, [id]: e.touches[0].clientX });
+      setDivY({ ...divY, [id]: e.touches[0].clientY });
     }
   };
 
@@ -106,75 +89,65 @@ function flashCard() {
   const positionFollowMouse = (e: React.MouseEvent<HTMLDivElement>) => {
     if (MouseDownBool) {
       console.log(e.clientX, e.clientY);
-      const id = Number(e.currentTarget.id) - 1;
+      const id = Number(e.currentTarget.id);
 
       var back;
       if (id == 0) {
-        back = 3;
+        back = Layers;
       } else {
-        back = id - 1;
+        back = id;
       }
-      const funcY = setDivY[id];
-      const funcX = setDivX[id];
-
       if (Counter[id] > Counter[back] && SwipedBool[id] == false) {
-        funcX(e.clientX);
-        funcY(e.clientY);
+        setDivX({ ...divX, [id]: e.clientX });
+        setDivY({ ...divY, [id]: e.clientY });
       }
     }
   };
 
   //Swipe animations
 
-  const Swipe = (rawID: number) => {
-    const id = rawID - 1;
-
-    const funcBool = setSwipedBool[id];
-    const funcDivX = setDivX[id];
-    const funcDivY = setDivY[id];
-    const funcCounter = setCounter[id];
-
-    funcBool(true);
+  const Swipe = (id: number) => {
+    setSwipedBool({ id: true });
 
     var SlideInterval: number;
-    var InternalCounterX = DivX[id];
-    var InternalCounterY = DivY[id];
+    var InternalCounterX = divX[id];
+    var InternalCounterY = divY[id];
 
     //Move flash card on one of the four diagonals depending on quadrile
-    if (DivX[id] > window.innerWidth / 2 && DivY[id] > window.innerHeight / 2) {
+    if (divX[id] > window.innerWidth / 2 && divY[id] > window.innerHeight / 2) {
       SlideInterval = setInterval(() => {
-        funcDivX(InternalCounterX + 1);
-        funcDivY(InternalCounterY + 1);
+        setDivX({ ...divX, [id]: InternalCounterX });
+        setDivY({ ...divY, [id]: InternalCounterY });
         InternalCounterX += 1;
         InternalCounterY += 1;
       }, 10);
     } else if (
-      DivX[id] < window.innerWidth / 2 &&
-      DivY[id] < window.innerHeight / 2
+      divX[id] < window.innerWidth / 2 &&
+      divY[id] < window.innerHeight / 2
     ) {
       SlideInterval = setInterval(() => {
-        funcDivX(InternalCounterX - 1);
-        funcDivY(InternalCounterY - 1);
+        setDivX({ ...divX, [id]: InternalCounterX });
+        setDivY({ ...divY, [id]: InternalCounterY });
         InternalCounterX -= 1;
         InternalCounterY -= 1;
       }, 10);
     } else if (
-      DivX[id] > window.innerWidth / 2 &&
-      DivY[id] < window.innerHeight / 2
+      divX[id] > window.innerWidth / 2 &&
+      divY[id] < window.innerHeight / 2
     ) {
       SlideInterval = setInterval(() => {
-        funcDivX(InternalCounterX + 1);
-        funcDivY(InternalCounterY - 1);
+        setDivX({ ...divX, [id]: InternalCounterX });
+        setDivY({ ...divY, [id]: InternalCounterY });
         InternalCounterX += 1;
         InternalCounterY -= 1;
       }, 10);
     } else if (
-      DivX[id] < window.innerWidth / 2 &&
-      DivY[id] > window.innerHeight / 2
+      divX[id] < window.innerWidth / 2 &&
+      divY[id] > window.innerHeight / 2
     ) {
       SlideInterval = setInterval(() => {
-        funcDivX(InternalCounterX - 1);
-        funcDivY(InternalCounterY + 1);
+        setDivX({ ...divX, [id]: InternalCounterX });
+        setDivY({ ...divY, [id]: InternalCounterY });
         InternalCounterX -= 1;
         InternalCounterY += 1;
       }, 10);
@@ -183,188 +156,60 @@ function flashCard() {
     //Once animation is done
     setTimeout(() => {
       clearInterval(SlideInterval);
-      funcBool(false);
-      fetchRandomPhoto(id);
-      funcDivX(window.innerWidth / 2);
-      funcDivY(window.innerHeight / 2);
+      setSwipedBool({ ...SwipedBool, id: false });
+      fetchRandomPhoto(String(id));
+      setDivX({ ...divX, [id]: window.innerWidth / 2 });
+      setDivY({ ...divY, [id]: window.innerHeight / 2 });
       // setImgCounterF(ImgCounterF + 2);
-      funcCounter(Counter[id] - 2);
+      setCounter({ ...Counter, [id]: [Counter[id] - 2] });
     }, 1000);
   };
   return (
     <div className="body">
-      <div
-        id="1"
-        className={`flashCardDiv ${SwipedBool1 ? "FadeOut" : ""}`}
-        onTouchMove={(e) => {
-          positionFollowTouch(e);
-        }}
-        onTouchEnd={() => {
-          Swipe(1);
-        }}
-        onMouseDown={(e) => {
-          positionFollowMouse(e), setMDBool(true);
-        }}
-        onMouseMove={(e) => {
-          positionFollowMouse(e);
-        }}
-        onMouseUp={(e) => {
-          positionFollowMouse(e), setMDBool(false), Swipe(1);
-        }}
-        style={{ top: divY1, left: divX1, zIndex: Counter1 }}
-      >
-        <div
-          style={{
-            backgroundImage: `url("${photoUrl1}")`,
-            backgroundPosition: "center",
-            backgroundSize: "cover",
-            filter: "blur(8px)",
-            width: "100%",
-            height: "100%",
-          }}
-        ></div>
-        <img
-          src={photoUrl1}
-          width={window.innerWidth}
-          height={window.innerHeight}
-          draggable="false"
-          /**        onError={() => {
-            fetchRandomPhoto(0);
-          }}
-*/
-        ></img>
-      </div>
-      <div
-        id="2"
-        className={`flashCardDiv ${SwipedBool2 ? "FadeOut" : ""}`}
-        onTouchMove={(e) => {
-          positionFollowTouch(e);
-        }}
-        onTouchEnd={() => {
-          Swipe(2);
-        }}
-        onMouseDown={(e) => {
-          positionFollowMouse(e), setMDBool(true);
-        }}
-        onMouseMove={(e) => {
-          positionFollowMouse(e);
-        }}
-        onMouseUp={(e) => {
-          positionFollowMouse(e), setMDBool(false), Swipe(2);
-        }}
-        style={{
-          top: divY2,
-          left: divX2,
-          zIndex: Counter2,
-        }}
-      >
-        <div
-          style={{
-            backgroundImage: `url("${photoUrl2}")`,
-            backgroundPosition: "center",
-            backgroundSize: "cover",
-            filter: "blur(8px)",
-            width: "100%",
-            height: "100%",
-          }}
-        ></div>
-        <img
-          src={photoUrl2}
-          width={window.innerWidth}
-          height={window.innerHeight}
-          draggable="false"
-          /**        onError={() => {
-            fetchRandomPhoto(0);
-          }}
-*/
-        ></img>
-      </div>
-      <div
-        id="3"
-        className={`flashCardDiv ${SwipedBool3 ? "FadeOut" : ""}`}
-        onTouchMove={(e) => {
-          positionFollowTouch(e);
-        }}
-        onTouchEnd={() => {
-          Swipe(3);
-        }}
-        onMouseDown={(e) => {
-          positionFollowMouse(e), setMDBool(true);
-        }}
-        onMouseMove={(e) => {
-          positionFollowMouse(e);
-        }}
-        onMouseUp={(e) => {
-          positionFollowMouse(e), setMDBool(false), Swipe(3);
-        }}
-        style={{ top: divY3, left: divX3, zIndex: Counter3 }}
-      >
-        <div
-          style={{
-            backgroundImage: `url("${photoUrl3}")`,
-            backgroundPosition: "center",
-            backgroundSize: "cover",
-            filter: "blur(8px)",
-            width: "100%",
-            height: "100%",
-          }}
-        ></div>
-        <img
-          src={photoUrl3}
-          width={window.innerWidth}
-          height={window.innerHeight}
-          draggable="false"
-          /**        onError={() => {
-            fetchRandomPhoto(0);
-          }}
-*/
-        ></img>
-      </div>
-      <div
-        id="4"
-        className={`flashCardDiv ${SwipedBool4 ? "FadeOut" : ""}`}
-        onTouchMove={(e) => {
-          positionFollowTouch(e);
-        }}
-        onTouchEnd={() => {
-          Swipe(4);
-        }}
-        onMouseDown={(e) => {
-          positionFollowMouse(e), setMDBool(true);
-        }}
-        onMouseMove={(e) => {
-          positionFollowMouse(e);
-        }}
-        onMouseUp={(e) => {
-          positionFollowMouse(e), setMDBool(false), Swipe(4);
-        }}
-        style={{
-          top: divY4,
-          left: divX4,
-          zIndex: Counter4,
-        }}
-      >
-        <div
-          style={{
-            backgroundImage: `url("${photoUrl4}")`,
-            backgroundPosition: "center",
-            backgroundSize: "cover",
-            filter: "blur(8px)",
-            width: "100%",
-            height: "100%",
-          }}
-        ></div>
-        <img
-          src={photoUrl4}
-          width={window.innerWidth}
-          height={window.innerHeight}
-          draggable="false"
-          /**        onError={() => {
-            fetchRandomPhoto(0);
-          }}
-*/
-        ></img>
-      </div>
+      {[...Array(Layers)].map((Layer, i) => {
+        return (
+          <div
+            id={String(i)}
+            className={`flashCardDiv ${SwipedBool[i] ? "FadeOut" : ""}`}
+            onTouchMove={(e) => {
+              positionFollowTouch(e);
+            }}
+            onTouchEnd={() => {
+              Swipe(i);
+            }}
+            onMouseDown={(e) => {
+              positionFollowMouse(e), setMDBool(true);
+            }}
+            onMouseMove={(e) => {
+              positionFollowMouse(e);
+            }}
+            onMouseUp={(e) => {
+              positionFollowMouse(e), setMDBool(false), Swipe(i);
+            }}
+            style={{ top: divY[i], left: divX[i], zIndex: Counter[i] }}
+          >
+            <div
+              style={{
+                backgroundImage: `url("${photoUrl[i]}")`,
+                backgroundPosition: "center",
+                backgroundSize: "cover",
+                filter: "blur(8px)",
+                width: "100%",
+                height: "100%",
+              }}
+            ></div>
+            <img
+              src={photoUrl[i]}
+              width={window.innerWidth}
+              height={window.innerHeight}
+              draggable="false"
+              onError={() => {
+                fetchRandomPhoto(String(i));
+              }}
+            ></img>
+          </div>
+        );
+      })}
     </div>
   );
 }
