461ae7b7017857a7ec12f7f2b6e7406022e496d6
CaptainSquid9 <83174650+CaptainSquid9@users.noreply.github.com>
2024-08-03 16:06:43 -0400

    Fix

--- node_modules/react-app/src/main.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import FlashCard from "./flashcard";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(<FlashCard />);
